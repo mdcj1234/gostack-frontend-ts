b87a54f82342f140ca1f2edcbbe0b43087307438
Marcio Junior <mdcj1234@gmail.com>
2020-09-03 12:58:41 -0300

    ajustes no index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-import Route from './Route';
-
-import SignIn from '../pages/SignIn';
-import ForgotPassword from '../pages/ForgotPassword';
-import ResetPassword from '../pages/ResetPassword';
-import SignUp from '../pages/SignUp';
-import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={SignIn} />
-    <Route path="/signup" component={SignUp} />
-    <Route path="/forgot-password" component={ForgotPassword} />
-    <Route path="/reset-password" component={ResetPassword} />
-
-    <Route path="/dashboard" component={Dashboard} isPrivate />
-    <Route path="/profile" component={Profile} isPrivate />
+    <Route path="/" exact component={Dashboard} />
   </Switch>
 );
 
